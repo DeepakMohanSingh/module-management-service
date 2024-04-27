5ed8c03853f1153adeaf250ae4fc413b431da7ae
Deepak Mohan Singh <p.s.vishalsingh@gmail.com>
2024-04-27 20:44:12 +0100

    changed exception message

--- src/main/java/com/tus/module/constant/ModuleConstants.java
+++ src/main/java/com/tus/module/constant/ModuleConstants.java
@@ -4,5 +4,5 @@ public class ModuleConstants {
 	private ModuleConstants() {
 	}
 
-	public static final String MODULE_NOT_FOUND = "A module with the given ID '%s' doesn't exist. Please verify the ID provided.";
+	public static final String MODULE_NOT_FOUND = "Testing the automated build.";
 }
