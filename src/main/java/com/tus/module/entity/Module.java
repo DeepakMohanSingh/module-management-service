e17980db94a461f93cdf6eb56c590a3ee4fdfe87
Deepak Mohan Singh <p.s.vishalsingh@gmail.com>
2024-04-27 12:27:11 +0100

    unit testing

--- src/main/java/com/tus/module/entity/Module.java
+++ src/main/java/com/tus/module/entity/Module.java
@@ -6,9 +6,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class Module {
 	@Id
